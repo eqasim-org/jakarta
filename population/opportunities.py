cf3e498c7e78147c4bf8d98fefb224e7968037c0
balacmi <milos.balac@ivt.baug.ethz.ch>
2020-05-17 09:29:03 +0200

    ading code to extract roads from OSM

--- population/opportunities.py
+++ population/opportunities.py
@@ -10,40 +10,21 @@ def configure(context, require):
     require.stage("data.spatial.zones")
 
 def execute(context):
-    df = pd.read_csv("%s/facilities_new.csv" % context.config["raw_data_path"])
-    df.columns = ["x", "y", "purpose"]
-    #df = df[df["purpose"] == "shop"]    
-    del df["purpose"]
-
-    df_opportunities = df
-
-        
-    #df_opportunities = pd.DataFrame(context.stage("data.bpe.cleaned")[[
-    #    "bpe_id", "x", "y", "activity_type", "commune_id"
-    #]], copy = True)
-    #df_opportunities.columns = ["location_id", "x", "y", "activity_type", "commune_id"]
-
+    df_opportunities = context.stage("data.opportunities.extract_roads_osm") 
+    df_opportunities = df_opportunities[["x", "y"]]
+    df_opportunities.columns = ["x", "y"]    
+    
     df_opportunities["offers_work"] = True
     df_opportunities["offers_other"] = True
-
-    df_opportunities["offers_leisure"] = True #df_opportunities["activity_type"] == "leisure"
-    df_opportunities["offers_shop"] = True #df_opportunities["activity_type"] == "shop"
-    df_opportunities["offers_education"] = True #df_opportunities["activity_type"] == "education"
-
+    df_opportunities["offers_leisure"] = True
+    df_opportunities["offers_shop"] = True 
+    df_opportunities["offers_education"] = False
     df_opportunities["offers_home"] = True
-
-    df_opportunities["location_id"] = np.arange(len(df_opportunities))
-
-    #df_zones = context.stage("data.spatial.zones")[["zone_id", "commune_id", "zone_level"]]
-    #df_zones = df_zones[df_zones["zone_level"] == "commune"][["zone_id", "commune_id"]]
-    #df_opportunities = pd.merge(df_opportunities, df_zones, on = "commune_id")
-
+   
     df_zones = context.stage("data.spatial.zones")
-    #print(df_zones.count)
-    #exit()
 
-    df_opportunities = data.spatial.utils.to_gpd(df_opportunities, crs = {"init" : "EPSG:5330"})
+    df_opportunities = data.spatial.utils.to_gpd(df_opportunities, crs = {"init" : "EPSG:4326"}).to_crs({"init" : "EPSG:5330"})
+    df_opportunities["location_id"] = np.arange(len(df_opportunities))
     df_opportunities = data.spatial.utils.impute(df_opportunities, df_zones, "location_id", "zone_id", fix_by_distance = False).dropna()
-
     
     return df_opportunities
